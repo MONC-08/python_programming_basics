a78a63dbff4ea9a6a854eb52a2737404c480722c
Chowdhury <md.nasif008@gmail.com>
2026-07-15 03:09:16 +0200

    string formatting

---
+++ build_a_simple_menu.py
@@ -0,0 +1,8 @@
+# Build a menu
+
+title = "menu".upper()
+print(title.center(21, "="))
+
+print("Coffee".ljust(16, ".") + "$1.50".rjust(4))
+print("Muffin".ljust(16, ".") + "$2.00".rjust(4))
+print("Cheesecake".ljust(16, ".") + "$2.50".rjust(4))
